683b506715c2caf8c8998475e3823948b577bd31
OmarNabil005 <omarnabil219@gmail.com>
2024-08-31 21:32:07 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
