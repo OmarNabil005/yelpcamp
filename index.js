d9ecf263b6ad10fc34fe8a0e8750513c7e567be3
OmarNabil005 <omarnabil219@gmail.com>
2024-09-01 14:02:39 +0300

    seeded the DB

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', () => {
+    resizeBy.send("Hello There");
+})
+
+app.get('/makeCampground', async (req, res) => {
+    const camp = new Campground({ title: "Egy", price: "200B$", description: "Hell on Earth", location: "North East Africa" });
+    await camp.save();
+    console.log(camp);
+    res.send("Done");
+})
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
---
+++ seeds/index.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; ++i) {
+        const rand1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${sample(places)} ${sample(descriptors)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close;
+});
